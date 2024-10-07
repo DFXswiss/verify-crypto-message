c1b677a48b51f6984b3e607db3467ba07cbb7403
lapatric <42653152+lapatric@users.noreply.github.com>
2024-10-07 19:35:51 +0200

    [DEV-2524] add loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams } from "react-router-dom";
 
 type FormData = {
   address: string;
@@ -11,6 +11,7 @@ type FormData = {
 
 // http://localhost:5173/?address=0xee0b53ac9578cbc6e0cee0ac6e18cfb7118b5cb3&message=Verifying%20I%27m%20chaskin.eth%20reaching%20out%20to%20Juani&signature=0x8af6bfa7a4cb8f6195751b714940dea4b9d72a2e03508bb1f61c47df4ff58e3f66b589d80344250b93ce49133bdeac813b72b41c93e78407cc96f0239b740b2f1c
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
   const [verificationResult, setVerificationResult] = useState<boolean>();
   const [error, setError] = useState<string | undefined>();
 
@@ -18,7 +19,7 @@ function App() {
 
   useEffect(() => {
     setUrlParams(new URLSearchParams());
-  }, [])
+  }, []);
 
   const {
     register,
@@ -41,26 +42,24 @@ function App() {
     setVerificationResult(undefined);
   });
 
-  const onSubmit: SubmitHandler<FormData> = (data) => {
+  async function onSubmit(data: FormData) {
     setError(undefined);
     setVerificationResult(undefined);
-    verifySignatureApi({ ...data });
-  };
 
-  function verifySignatureApi({ message, signature, address }: FormData) {
+    const { address, message, signature } = data;
     const encodedMessage = encodeURIComponent(message.trim());
     const url = `https://dev.api.dfx.swiss/v1/auth/verifySignature?address=${address}&message=${encodedMessage}&signature=${signature}`;
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        setVerificationResult(data.isValid);
-      })
-      .catch((error) => {
-        setError(error.toString());
-      });
-  }
+    try {
+      const response = await fetch(url);
+      const data_2 = await response.json();
+      setVerificationResult(data_2.isValid);
+    } catch (error: any) {
+      setError(error.toString());
+    }
+  };
 
   const handleValidationAndFocus = async () => {
+    setIsLoading(true);
     const isValid = await trigger();
     if (!isValid) {
       if (errors.address) {
@@ -72,7 +71,8 @@ function App() {
       }
     }
 
-    handleSubmit(onSubmit)();
+    await handleSubmit(onSubmit)();
+    setIsLoading(false);
   };
 
   return (
@@ -166,15 +166,30 @@ function App() {
               {error}
             </div>
           )}
-
           <button
             type="button"
-            className={`py-2 px-4 rounded-md text-white ${
-              isValid ? "bg-dfxRed-150" : "bg-slate-700"
-            }`}
+            disabled={isLoading}
+            className={`justify-center text-white rounded-lg px-5 py-2.5 inline-flex items-center ${isValid && !isLoading ? "bg-dfxRed-150 hover:bg-dfxRed-100 " : "bg-slate-700"}`}
             onClick={handleValidationAndFocus}
           >
-            Verify
+            {isLoading && <svg
+              aria-hidden="true"
+              role="status"
+              className="inline w-4 h-4 me-2 text-white animate-spin"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="#E5E7EB"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentColor"
+              />
+            </svg>}
+            {!isLoading ? "VERIFY" : "VERIFYING..."}
           </button>
 
           {verificationResult !== undefined && (
